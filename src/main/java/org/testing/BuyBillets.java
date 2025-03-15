ea6b3332934a8ef0fde77434ee7e042f658f1681
Stanislaw Tkachenko <stas1696@mail.ru>
2025-03-15 14:19:57 +0400

    Add new Project

---
+++ src/main/java/org/testing/BuyBillets.java
@@ -0,0 +1,22 @@
+package org.testing;
+
+import org.junit.Test;
+
+public class BuyBillets extends SourcePage {
+
+    @Test
+    public void findBillets() {
+        String dayTo = "10.06.2025";
+        String dayOut = "17.06.2025";
+        String whereCity = "Саратов";
+        String whereOutCity = "Москва";
+        ElementMethodsPage elementMethodsPage = new ElementMethodsPage(driver);
+        elementMethodsPage.findBilets(dayTo,
+                dayOut,
+                whereCity,
+                whereOutCity);
+
+
+
+    }
+}
